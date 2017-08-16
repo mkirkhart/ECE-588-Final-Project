ef80b3de797f4bd41597cdb47b60bcc1b0ce66b4
Michael Kirkhart <mkirkhar@umich.edu>
2017-08-15 22:48:39 -0400

    Enabled min-max depth image ranging, added trackbar window to set min and max depth

--- test-projects/opencv-kinect-example-refactor/src/test.cpp
+++ test-projects/opencv-kinect-example-refactor/src/test.cpp
@@ -8,8 +8,30 @@
 
 #include "MyFreenectDevice.h"
 
-using namespace cv;
-using namespace std;
+
+static int DepthLowerRange = 25;
+static int DepthUpperRange = 80;
+static const int DepthHighLimit = 300;
+
+static const char KinectDepthTrackbarWindowName[] = "Kinect Depth Detection Parameters";
+
+static const char KinectDepthTrackbarMinDepthName[] = "Min Depth";
+static const char KinectDepthTrackbarMaxDepthName[] = "Max Depth";
+
+
+static void KinectDepthDetectionCreateTrackbarWindow(void (*pCallbackFunction)(int, void *))
+{
+	cv::namedWindow(KinectDepthTrackbarWindowName, CV_WINDOW_AUTOSIZE);
+
+	cv::createTrackbar(KinectDepthTrackbarMinDepthName, KinectDepthTrackbarWindowName, &DepthLowerRange, DepthHighLimit, pCallbackFunction, NULL);
+	cv::createTrackbar(KinectDepthTrackbarMaxDepthName, KinectDepthTrackbarWindowName, &DepthUpperRange, DepthHighLimit, pCallbackFunction, NULL);
+}
+
+
+static void KinectDepthDetectDestroyTrackbarWindow(void)
+{
+	cv::destroyWindow(KinectDepthTrackbarWindowName);
+}
 
 
 int main(int argc, char **argv)
@@ -18,19 +40,14 @@ int main(int argc, char **argv)
 	const std::string RGBCaptureWindowName("RGB");
 	const std::string DepthCaptureWindowName("Depth");
 
-#ifdef _DEPTH_RANGE_MASKING
-	int DepthLowerRange = 25;
-	int DepthUpperRange = 80;
-#endif	//_DEPTH_RANGE_MASKING
-
-	string RGBFilename("rgb-snapshot");
-	string DepthFilename("depth-snapshot");
-	string suffix(".png");
+	std::string RGBFilename("rgb-snapshot");
+	std::string DepthFilename("depth-snapshot");
+	std::string suffix(".png");
 	int i_snap(0),iter(0);
 
-	Mat depthMat(Size(640,480),CV_16UC1);
-	Mat depthf (Size(640,480),CV_8UC1);
-	Mat rgbMat(Size(640,480),CV_8UC3,Scalar(0));
+	cv::Mat depthMat(cv::Size(640,480),CV_16UC1);
+	cv::Mat depthf (cv::Size(640,480),CV_8UC1);
+	cv::Mat rgbMat(cv::Size(640,480),CV_8UC3,cv::Scalar(0));
 
 	// The next two lines must be changed as Freenect::Freenect
 	// isn't a template but the method createDevice:
@@ -42,13 +59,13 @@ int main(int argc, char **argv)
 
 	// Well, apparently this is no longer true, and we need create the device
 	// via the "prior" method
-#ifdef _UBUNTU_12_04
+#ifdef _OLD_FREENECT_CPP_WRAPPER
 	Freenect::Freenect<MyFreenectDevice> freenect;
 	MyFreenectDevice& device = freenect.createDevice(0);
 #else
 	Freenect::Freenect freenect;
 	MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
-#endif	//_UBUNTU_12_04
+#endif	//_OLD_FREENECT_CPP_WRAPPER
 
 	namedWindow(RGBCaptureWindowName, CV_WINDOW_AUTOSIZE);
 	namedWindow(DepthCaptureWindowName, CV_WINDOW_AUTOSIZE);
@@ -62,6 +79,8 @@ int main(int argc, char **argv)
 	device.startVideo();
 	device.startDepth();
 
+	KinectDepthDetectionCreateTrackbarWindow(NULL);
+
 	while(!die)
 	{
 		device.getVideo(rgbMat);
@@ -71,13 +90,11 @@ int main(int argc, char **argv)
 		// This might need some adjustment after setting the depth format to FREENECT_DEPTH_REGISTERED
 		depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
 
-#ifdef _DEPTH_RANGE_MASKING
 		// show objects within given range as white, everything else as black
 		cv::inRange(depthf, DepthLowerRange, DepthUpperRange, depthf);
-#endif	//_DEPTH_RANGE_MASKING
 
 		cv::imshow(DepthCaptureWindowName, depthf);
-		char k = cvWaitKey(5);
+		char k = cv::waitKey(5);
 
 		switch(k)
 		{
@@ -117,11 +134,12 @@ int main(int argc, char **argv)
 
 	}
 
-	destroyWindow(RGBCaptureWindowName);
-	destroyWindow(DepthCaptureWindowName);
-
 	device.stopVideo();
 	device.stopDepth();
+
+	cv::destroyWindow(RGBCaptureWindowName);
+	cv::destroyWindow(DepthCaptureWindowName);
+	KinectDepthDetectDestroyTrackbarWindow();
 
 	return 0;
 }
